11251772dd724c0fe5bf3bdfac6cb36e308096cd
Alice Olle-Laprune <alice.ollelaprune@gmail.com>
2018-08-06 17:16:50 +0200

    popular event flatlist updated

--- src/containers/HomePagePopular/index.js
+++ src/containers/HomePagePopular/index.js
@@ -7,7 +7,8 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   StyleSheet,
-  SectionList
+  SectionList,
+  FlatList
 } from "react-native";
 import { withNavigation } from "react-navigation";
 
@@ -27,8 +28,7 @@ export default class HomePageUpcoming extends Component {
     events: [],
     page: 1,
     isLoading: true,
-    error: "",
-    isLoadingMore: false
+    error: ""
   };
 
   getEvents() {
@@ -41,9 +41,8 @@ export default class HomePageUpcoming extends Component {
       )
       .then(response => {
         this.setState({
-          events: [...this.state.events, ...response.data],
-          isLoading: false,
-          isLoadingMore: false
+          events: response.data,
+          isLoading: false
         });
       })
       .catch(function(error) {
@@ -52,62 +51,6 @@ export default class HomePageUpcoming extends Component {
         );
         throw error;
       });
-  }
-  // after reaching the end of the displayed events list, this function is called to display 50 more events.
-  handleLoadMore = () => {
-    if (!this.state.isLoadingMore) {
-      this.setState(
-        {
-          page: this.state.page + 1,
-          isLoadingMore: true
-        },
-        () => {
-          this.getEvents();
-        }
-      );
-    }
-  };
-
-  // Storing in an array all dates at which events are happening
-  getDates(events) {
-    dates = [];
-    for (let i = 0; i < events.length; i++) {
-      if (dates.indexOf(events[i].start.date) === -1) {
-        dates.push(events[i].start.date);
-      }
-    }
-    return dates;
-  }
-
-  // rendering in an array all events happening on a defined date
-  renderEventsCard(date, events) {
-    eventsByDate = [];
-    for (let i = 0; i < events.length; i++) {
-      if (events[i].start.date === date) {
-        eventsByDate.push(
-          <ConcertCard event={events[i]} navigate={this.props.navigate} />
-        );
-      }
-    }
-    return eventsByDate;
-  }
-
-  // creating object with adequate formatting for generating the section list
-  renderSectionListContent(events) {
-    let dates = this.getDates(events);
-    let sectionContents = [];
-    for (let i = 0; i < dates.length; i++) {
-      sectionContents.push({
-        title: dates[i],
-        data: this.renderEventsCard(dates[i], events)
-      });
-    }
-    return sectionContents;
-  }
-
-  // formating the date header before the list of events happening at this date
-  renderDateTitle(date) {
-    return moment(date).format("dddd") + ", " + moment(date).format("MMMM Do");
   }
 
   render() {
@@ -118,16 +61,49 @@ export default class HomePageUpcoming extends Component {
         </View>
       );
     } else {
-      sectionListData = this.renderSectionListContent(this.state.events);
-      // SectionList used with onEndReached for infinite scrolling
+      console.log("else");
       return (
-        <SectionList
+        <FlatList
           style={[
             {
-              paddingHorizontal: 10
+              padding: 10
             },
             styles.backgroundOfPage
           ]}
+          data={this.state.events}
+          keyExtractor={(item, index) => item.displayName}
+          renderItem={obj => {
+            console.log("item", obj);
+            return (
+              <ConcertCard event={obj.item} navigate={this.props.navigate} />
+            );
+          }}
+        />
+      );
+    }
+  }
+  componentDidMount() {
+    this.getEvents();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  },
+  backgroundOfPage: {
+    backgroundColor: "#F4F8FF"
+  }
+});
+
+/* <SectionList
+          
           renderItem={({ item, index, section }) => (
             <View key={index}>{item}</View>
           )}
@@ -153,28 +129,11 @@ export default class HomePageUpcoming extends Component {
           )}
           sections={sectionListData}
           keyExtractor={(item, index) => item + index}
-          onEndReached={this.handleLoadMore}
-          onEndReachedThreshold={100}
-        />
-      );
-    }
-  }
-  componentDidMount() {
-    this.getEvents();
-  }
-}
+        /> */
+/* <Flatlist
+          data={this.state.events}
+          keyExtractor={(item, index) => item}
+          renderItem={({ obj }) => <Text>{obj.title}</Text>}
+        /> */
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  horizontal: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 10
-  },
-  backgroundOfPage: {
-    backgroundColor: "#F4F8FF"
-  }
-});
+/* <ConcertCard event={obj.item} navigate={this.props.navigate} /> */
