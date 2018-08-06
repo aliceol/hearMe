385cfc359c5cf418c81c3553e660fdaa70c8c22d
Alice Olle-Laprune <alice.ollelaprune@gmail.com>
2018-08-06 15:37:11 +0200

    popular events page added

--- src/containers/HomePagePopular/index.js
+++ src/containers/HomePagePopular/index.js
@@ -1,53 +1,180 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import {
   Text,
   View,
   StatusBar,
   ScrollView,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator,
+  StyleSheet,
+  SectionList
 } from "react-native";
 import { withNavigation } from "react-navigation";
-export default class HomePagePopular extends Component {
+
+const moment = require("moment");
+moment().format();
+
+import axios from "axios";
+
+import ConcertCard from "../../components/ConcertCard";
+
+export default class HomePageUpcoming extends Component {
   static navigationOptions = {
-    title: "HomePagePopular"
+    title: "HomePageUpcoming"
   };
+
+  state = {
+    events: [],
+    page: 1,
+    isLoading: true,
+    error: "",
+    isLoadingMore: false
+  };
+
+  getEvents() {
+    axios
+      .get(
+        "https://hearme-api.herokuapp.com/api/city/popular/" +
+          this.props.route.cityCode +
+          "/" +
+          this.state.page
+      )
+      .then(response => {
+        this.setState({
+          events: [...this.state.events, ...response.data],
+          isLoading: false,
+          isLoadingMore: false
+        });
+      })
+      .catch(function(error) {
+        console.log(
+          "There has been a problem with your operation: " + error.message
+        );
+        throw error;
+      });
+  }
+  // after reaching the end of the displayed events list, this function is called to display 50 more events.
+  handleLoadMore = () => {
+    if (!this.state.isLoadingMore) {
+      this.setState(
+        {
+          page: this.state.page + 1,
+          isLoadingMore: true
+        },
+        () => {
+          this.getEvents();
+        }
+      );
+    }
+  };
+
+  // Storing in an array all dates at which events are happening
+  getDates(events) {
+    dates = [];
+    for (let i = 0; i < events.length; i++) {
+      if (dates.indexOf(events[i].start.date) === -1) {
+        dates.push(events[i].start.date);
+      }
+    }
+    return dates;
+  }
+
+  // rendering in an array all events happening on a defined date
+  renderEventsCard(date, events) {
+    eventsByDate = [];
+    for (let i = 0; i < events.length; i++) {
+      if (events[i].start.date === date) {
+        eventsByDate.push(
+          <ConcertCard event={events[i]} navigate={this.props.navigate} />
+        );
+      }
+    }
+    return eventsByDate;
+  }
+
+  // creating object with adequate formatting for generating the section list
+  renderSectionListContent(events) {
+    let dates = this.getDates(events);
+    let sectionContents = [];
+    for (let i = 0; i < dates.length; i++) {
+      sectionContents.push({
+        title: dates[i],
+        data: this.renderEventsCard(dates[i], events)
+      });
+    }
+    return sectionContents;
+  }
+
+  // formating the date header before the list of events happening at this date
+  renderDateTitle(date) {
+    return moment(date).format("dddd") + ", " + moment(date).format("MMMM Do");
+  }
+
   render() {
-    return (
-      <View>
-        <Text>This is the HomePagePopular Page</Text>
-        <TouchableOpacity
-          style={{ marginTop: 100 }}
-          onPress={() => {
-            this.props.navigation.navigate("EventPage");
-          }}
-        >
-          <Text style={{ fontSize: 10 }}>Go To EventPageScreen</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ marginTop: 100 }}
-          onPress={() => {
-            this.props.navigation.navigate("HomePageUpcoming");
-          }}
-        >
-          <Text style={{ fontSize: 10 }}>Go To HomePageUpcomingScreen</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ marginTop: 100 }}
-          onPress={() => {
-            this.props.navigation.navigate("MyLikes");
-          }}
-        >
-          <Text style={{ fontSize: 10 }}>Go To MyLikesScreen</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ marginTop: 100 }}
-          onPress={() => {
-            this.props.navigation.navigate("MyProfile");
-          }}
-        >
-          <Text style={{ fontSize: 10 }}>Go To MyProfileScreen</Text>
-        </TouchableOpacity>
-      </View>
-    );
+    if (this.state.isLoading) {
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
+    } else {
+      sectionListData = this.renderSectionListContent(this.state.events);
+      // SectionList used with onEndReached for infinite scrolling
+      return (
+        <SectionList
+          style={[
+            {
+              paddingHorizontal: 10
+            },
+            styles.backgroundOfPage
+          ]}
+          renderItem={({ item, index, section }) => (
+            <View key={index}>{item}</View>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text
+              style={[
+                {
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "100%",
+                  textAlign: "center",
+                  marginBottom: 10,
+                  paddingVertical: 5,
+                  fontWeight: "bold",
+                  color: "#2D3436"
+                },
+                styles.backgroundOfPage
+              ]}
+            >
+              {this.renderDateTitle(title)}
+            </Text>
+          )}
+          sections={sectionListData}
+          keyExtractor={(item, index) => item + index}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={100}
+        />
+      );
+    }
+  }
+  componentDidMount() {
+    this.getEvents();
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  },
+  backgroundOfPage: {
+    backgroundColor: "#F4F8FF"
+  }
+});
