181a1ca2dd783dd9f9b916b0ee08506de9cc5f0a
Yacha Toueg <yachatoueg@Yachas-MacBook-Pro.local>
2018-08-09 13:58:26 +0200

    user token avatar

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -4,11 +4,13 @@ import React from "react";
 import { StyleSheet, Image, TouchableOpacity } from "react-native";
 import ImagePicker from "react-native-image-picker";
 import axios from "axios";
+import store from "react-native-simple-store";
 
 class Avatar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      files: [],
       avatar: require("../../images/ic_tag_faces.png")
     };
     // this.setState est appelé dans un callback dans showImagePicker, pensez donc bien à binder la fonction _avatarClicked
@@ -24,22 +26,40 @@ class Avatar extends React.Component {
       } else {
         console.log("Photo : ", response.uri);
         let source = { uri: "data:image/jpeg;base64," + response.data };
+
         console.log(source);
-        this.setState({
-          avatar: source
-        });
-        UpdatePhoto = () => {
-          axios
-            .post("https://hearme-api.herokuapp.com/api/user/uploadPicture", {
-              ...this.state //copie du state
-            })
-            .then(response => {
-              conosle.log(response);
-            })
-            .catch(err => {
-              console.log(err);
+        let sourceBase64 = "data:image/jpeg;base64," + response.data;
+        console.log(sourceBase64);
+        this.setState(
+          {
+            avatar: source,
+            files: [sourceBase64]
+          },
+          () => {
+            store.get("userToken").then(res => {
+              console.log(res.token);
+              const config = {
+                headers: {
+                  Authorization: "Bearer " + res.token
+                }
+              };
+              axios
+                .post(
+                  "http://localhost:3000/api/user/uploadPicture",
+                  {
+                    ...this.state //copie du state
+                  },
+                  config
+                )
+                .then(response => {
+                  console.log(response);
+                })
+                .catch(err => {
+                  console.log(err);
+                });
             });
-        };
+          }
+        );
       }
     });
   }
